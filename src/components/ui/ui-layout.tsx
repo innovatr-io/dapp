364ca3f0b97d0d0c8a18df3fa87276ed6d638a68
Jo Vinkenroye <jestersimpps@gmail.com>
2024-11-23 15:16:36 +0100

    fix: Adjust max-width calculation for mobile content overflow issue

--- src/components/ui/ui-layout.tsx
+++ src/components/ui/ui-layout.tsx
@@ -164,7 +164,7 @@ export function UiLayout({
               <AccountChecker />
             </ClusterChecker>
           </div>
-          <main className="flex-1 p-4 lg:p-8 max-w-[100vw-16rem] lg:max-w-[calc(100vw-16rem)]">
+          <main className="flex-1 p-4 lg:p-8 max-w-[calc(100vw-1rem)] lg:max-w-[calc(100vw-16rem)]">
           <Suspense
             fallback={
               <div className="text-center my-32">

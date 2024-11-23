bf1f8d2c02cdd11c6d356f23b8a266e40ee25af3
Jo Vinkenroye <jestersimpps@gmail.com>
2024-11-23 13:49:58 +0100

    style: Make gap between featured projects consistent with padding adjustments

--- src/components/market/components/featured-projects.tsx
+++ src/components/market/components/featured-projects.tsx
@@ -64,7 +64,7 @@ export function FeaturedProjects({ projects }: { projects: Project[] }) {
       <div id="featured-carousel" className="carousel rounded-box relative group">
         <div className="absolute inset-y-0 left-0 bg-gradient-to-r from-base-100 to-transparent w-12 z-10 pointer-events-none opacity-0 group-hover:opacity-100 transition-opacity"></div>
         {featuredProjects.map((project) => (
-          <div key={project.id} className="carousel-item w-full md:w-1/2 lg:w-1/3 pr-4">
+          <div key={project.id} className="carousel-item w-full md:w-1/2 lg:w-1/3 px-2">
             <ProjectCard project={project} featured />
           </div>
         ))}

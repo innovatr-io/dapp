eb4855db515bfa594fca66e528138210c957d0c0
Jo Vinkenroye <jestersimpps@gmail.com>
2024-11-23 18:27:21 +0100

    pl

--- src/components/innovatr/innovatr-ui.tsx
+++ src/components/innovatr/innovatr-ui.tsx
@@ -63,6 +63,7 @@ function InnovatrCard({ account }: { account: PublicKey }) {
   return accountQuery.isLoading ? (
     <span className="loading loading-spinner loading-lg"></span>
   ) : (
+    
     <div className="card card-bordered border-base-300 border-4 text-neutral-content">
       <div className="card-body items-center text-center">
         <div className="space-y-6">
